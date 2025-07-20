7262113d3c504c71054f910a4600e7d05bab26ec
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-07-20 09:10:10 +0530

    ollama changes

--- week1/day2.py
+++ week1/day2.py
@@ -30,12 +30,12 @@ payload = {
 # !ollama pull llama3.2
 
 response = requests.post(OLLAMA_API, json=payload, headers=HEADERS)
-print(response.json()['message']['content'])
+print(f"{MODEL} -> {response.json()['message']['content']}")
 
 import ollama
 
 response = ollama.chat(model=MODEL, messages=messages)
-print(response['message']['content'])
+print(f"{MODEL} -> {response['message']['content']}")
 
 # There's actually an alternative approach that some people might prefer
 # You can use the OpenAI client python library to call Ollama:
@@ -48,8 +48,7 @@ response = ollama_via_openai.chat.completions.create(
     messages=messages
 )
 
-print(response.choices[0].message.content)
-
+print(f"{MODEL} -> {response.choices[0].message.content}")
 # This may take a few minutes to run! You should then see a fascinating "thinking" trace inside <think> tags, followed by some decent definitions
 # !ollama pull deepseek-r1:1.5b
 MODEL = "deepseek-r1:1.5b"
@@ -59,4 +58,4 @@ response = ollama_via_openai.chat.completions.create(
     messages=[{"role": "user", "content": "Please give definitions of some core concepts behind LLMs: a neural network, attention and the transformer"}]
 )
 
-print(response.choices[0].message.content)
+print(f"{MODEL} -> {response.choices[0].message.content}")
